e24276389c3a44ddb7a494e2f0bb0f6ad85a05a0
leannann <terzi_andrey_sergeevich@mail.ru>
2025-08-28 04:55:34 +0300

    REWRITED CHANGESETS

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -1,5 +1,7 @@
 package pro.sky.telegrambot.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.model.NotificationTask;
